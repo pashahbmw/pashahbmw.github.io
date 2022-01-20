1b53953228230252fa20b3bd1d37d3aa7ee9326d
pashahbmw <98105630+pashahbmw@users.noreply.github.com>
2022-01-20 22:59:48 +0700

    Update myJs.js

--- js/myJs.js
+++ js/myJs.js
@@ -138,7 +138,7 @@ $(document).ready(function () {
           text: textConfig.text11,
           confirmButtonColor: "#83d0c9",
           onClose: () => {
-            window.location = "https://www.facebook.com/messages/t/100001668862991";
+            window.location = "https://www.facebook.com/messages/t/HAHPAS.bmw/";
           },
         });
       }
